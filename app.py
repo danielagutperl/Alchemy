3d2045462a86d922a8ed128872c7086d6f2d9dea
danielagutperl <daniela.gutperl@gmail.com>
2019-09-25 18:43:00 +0100

    fixing deployment issues

--- app.py
+++ app.py
@@ -16,24 +16,5 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 
-# from models.drink import Drink, DrinkSchema
-# this has to go after ma ^^
-# drink_schema = DrinkSchema(many=True)
-# TODO: I dont think I need this here any more..? 
-
-# FOR TESTING:
-# # @app.route('/')
-# # def home():
-# #     return 'Heya!', 200
-#BECOMES THIS:
-# @app.route('/drinks')
-# def index():
-#     # select all drinks and store in a variable:
-#     drinks = Drink.query.all()
-#     # bring back the jsonified (from ma) drink_schema with all the drinks from ^^ :
-#     return drink_schema.jsonify(drinks), 200
-#     # many=True, because by default it wont know to expect many (but it'll get many so will respond with 200 and an empty object)
-#MOVED TO CONTROLLERS.drinks
-
 
 from config import router
--- config/env.py
+++ config/env.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5000/alchemy')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/alchemy')
 secret = os.getenv('SECRET', 'secretsquirrelchinesewhispers')
